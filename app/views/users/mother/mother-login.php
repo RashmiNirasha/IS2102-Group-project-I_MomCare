c02aa324ebd5dada812c3279c8e4739bd39b3363
bimbi.wickz <bimbi2254@gmail.com>
2022-12-13 10:51:09 +0530

    updated mother UIs

---
+++ app/views/users/mother/mother-login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login_Mother</title>
+    
+    <link rel="stylesheet" href="login.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<body>
+    <div class="logo">
+        <img src="../Assets/Project Logo-01.png" alt="logo">
+    </div>
+    <div class="bg-image">
+        <img src="../Assets/jonathan-borba--W1-1nSZJw8-unsplash.jpg" alt="bg image">
+    </div>
+    <h1 class="heading">Hello there!</h1>
+    <div class="vline"></div>
+    <div class="login">
+        <h2>Sign In</h2>
+        <div class="login-form">
+            <table class="login-details">
+                <tr>
+                    <td><label for="mother-username">Username</label></td>
+                    <td>
+                        <div class="input-fields">
+                            <input type="text" id="mother-username" placeholder="">
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="mother-password">Password</label></td>
+                    <td>
+                        <div class="input-fields">
+                            <input type="password" id="mother-password" placeholder="">
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="forgot-pass"><a href="">Forgot Password</a></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <div class="btn">
+                            <input type="button" value="Sign In">
+                            <input type="button" value="Back">
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
