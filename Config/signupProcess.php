a0299e75bdf8b751e87c361a9a6576c4da31a2a5
ShermilaPKSS <2020is100@stu.ucsc.cmb.ac.lk>
2022-12-17 20:18:37 +0530

    admin signup | updated - config

--- Config/signupProcess.php
+++ Config/signupProcess.php
@@ -1,65 +1,63 @@
 <?php
-//Connecting to the database
+//Connect to the database
 require_once "dbConnection.php";
 
-//Check Whether all the keys have a value
+//Function Definintion to check the array is empty
 function is_array_empty($arr){
-    if (is_array($arr)){
-        foreach ($arr as $value){
-            if (empty($value)){
-                return false;
+        if (is_array($arr)){
+            foreach ($arr as $value){
+                if (empty($value)){
+                    return 0;
+                }
             }
         }
-    }
-    return true;
+        return 1;
 }
 
-//call the is_array_empty fuction
-$check_not_null = is_array_empty($_POST);
-if ($check_not_null == true){
-        if (isset($_POST['insert'])){
+//Check whether all the keys paired with a value
+$isNotNull = is_array_empty($_POST);
+print_r($_POST);
+echo "<br> $isNotNull";
+
+if ($isNotNull == 1){
+    if (isset($_POST['insert'])){
+        if (isset($_POST['role']) && ($_POST['role'] == 'admin')){
             $email = $_POST['email'];
-            if ($_POST['role'] == 'admin'){
-                $sql_check = "SELECT COUNT('ad_email') as 'email_count' FROM admin WHERE ad_email='$email'";
-                $check = $con->query($sql_check);
-                if ($check->num_rows>0){
-                    while ($row = $check->fetch_assoc()){
-                        $is_email_exist = $row['email_count'];
-                        if ($is_email_exist == 0){
-                            if (strlen($_POST['password']) >= 6){
-                                if ($_POST['password'] == $_POST['confirmpass']){
-                                    $password = md5($_POST['password']);
-                                }elseif($_POST['password'] != $_POST['confirmpass']){
-                                    header("Location:..\View\Admin\signup.php?error=confirmPasswordError");
-                                }
-                            }else{
-                                header("Location:..\View\Admin\signup.php?error=passLength");
-                            }
-                        }else{
-                            header("Location:..\View\Admin\signup.php?error=emailExist");
-                        }
-                    }
+            
+            $sql_check = "SELECT COUNT('ad_email') as 'email_count' FROM admin WHERE ad_email='$email'";
+            $check = $con->query($sql_check);
+            print_r($check);
+
+            if ($check->num_rows>0){
+                while ($row = $check->fetch_assoc()){
+                    print_r($row);
+                    $is_email_exist = $row['email_count'];
                 }
-
-
-            $sql_insert = "INSERT INTO admin (ad_passwd, ad_email) VALUES('$password', '$email')";
-            $insert_admin = $con->query($sql_insert);
-            if ($insert_admin){
-                header("Location:..\View\Admin\signup.php?status=success");
             }
+            if ($is_email_exist != 1){
+                if (strlen($_POST['password']) >= 6){
+                    if ($_POST['password'] == $_POST['confirmpass']){
+                        $password = md5($_POST['password']);
 
-        }
-    }//elseif($_POST['role'] == 'mother'){ header(Location:)}
-    
-}elseif ($check_not_null == false){
+                        $sql_insert = "INSERT INTO admin (ad_passwd, ad_email) VALUES('$password', '$email')";
+                        $insert_admin = $con->query($sql_insert);
+                        if ($insert_admin){
+                            header("Location:..\View\Admin\signup.php?status=success");
+                        }
+
+                    }else{
+                        header("Location:..\View\Admin\signup.php?error=confirmPasswordError");
+                    }
+                }else{
+                    header("Location:..\View\Admin\signup.php?error=passLength");
+                }
+            }else{
+                header("Location:..\View\Admin\signup.php?error=emailExist");
+            }
+        }// else of role != admin
+        
+    }// else of insert not set
+}else{
     header("Location:..\View\Admin\signup.php?error=emptyFields");
 }
-
-
-
-
-
-
-
-
 ?>
