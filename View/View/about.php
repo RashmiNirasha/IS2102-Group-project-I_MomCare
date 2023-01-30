24d30b1099dd63e0b03e4df21ef213aaf0b26b72
Rashmi Gunawardana <90468021+RashmiNirasha@users.noreply.github.com>
2023-01-30 19:53:07 +0530

    Added common pages

---
+++ View/View/about.php
@@ -0,0 +1,35 @@
+<?php include "../Includes/header.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style><?php include "../Css/style.css" ?></style>
+</head>
+<body>
+<div class="landingMain">
+        <div class="landingLeft">
+            <img class="landingPagePhoto" src="../Assets/images/common/landing-page-photo.jpg" alt="landing-page-photo">
+        </div>
+        <div class="landingRight">
+            <div class="rightContent">
+                <h1>About <br> MOM CARE</h1>
+            </div>
+            <div class="aboutContent">
+                <p>As the youth striving for the betterment of the world, 
+                    we are concerned about Sri Lanka's maternal healthcare 
+                    well-being, since its quality has a significant bearing 
+                    on the fundamental growth of the future to be born. 
+                    <br><br>
+                    Through our platform, we intend to facilitate our consumers 
+                    with an easy and effective means of raising the standard of 
+                    both maternal healthcare and early childhood health and 
+                    development services across the nation.</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+<?php include "../Includes/footer.php"; ?>
