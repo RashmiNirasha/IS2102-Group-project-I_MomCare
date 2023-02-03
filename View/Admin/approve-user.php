36d1efdcbf62869de6359aa1fd2e8c846b0f5822
Rashmi Gunawardana <90468021+RashmiNirasha@users.noreply.github.com>
2023-02-03 12:47:35 +0530

    ok

---
+++ View/Admin/approve-user.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <style><?php include "Assets/css/style-common.css" ?></style> 
+    </head>
+    <body>
+       <div class="approveUser">
+            <table>
+                <tr>
+                    <td>Assign Username</td>
+                </tr>
+                <tr>
+                    <td>Assign Password</td>
+                </tr>
+                <tr>
+                    <td><button>Generate mail</button></td>
+                </tr>
+            </table>
+        </div>
+       
+    </body>
