f8804715d1eec4bd18dd00e5f150e44668922184
Rashmi Gunawardana <90468021+RashmiNirasha@users.noreply.github.com>
2023-02-07 00:53:10 +0530

    ok

--- Config/ped-notesTableModel.php
+++ Config/ped-notesTableModel.php
@@ -8,7 +8,8 @@ $result = mysqli_query($con, $sql);
 
 if($result){
     while($row = mysqli_fetch_assoc($result)){
-        $ped_note_id = $row['ped_note_id'];
+        $child_id=$row['child_id'];
+        $ped_note_id=$row['ped_note_id'];
         $doc_id = $row['doc_id'];
         $mom_id = $row['mom_id'];
         $note_topic = $row['note_topic'];
@@ -16,7 +17,7 @@ if($result){
         $note_description = $row['note_description'];
         $note_records = $row['note_records'];
         echo '<tr>
-        <th scope="row"> ' . $ped_note_id .'</th>
+        <th scope="row"> ' . $child_id .'</th>
         <td>' . $doc_id .'</td>
         <td>' . $mom_id .'</td>
         <td>' . $note_topic .'</td>
@@ -24,9 +25,8 @@ if($result){
         <td>' . $note_date .'</td>
         <td>' . $note_records .'</td>
         <td>
-        <a href="pediatrician-updateNotesView.php?updateid='.$ped_note_id.'">Update</a>
-        <a href="delete.php?deleteid='.$ped_note_id.'">Delete</a>
-        </td>
+        <a href="pediatrician-updateNotesView.php?updateid='.$ped_note_id.'"><button class="btnMain">Update</button></a></td>
+        <td><a href="delete.php?deleteid='.$ped_note_id.'"><button class="btnMain">Delete</button></a></td>
         </tr> ';
     }
 }
--- View/Pediatrician/pediatrician-viewNotesView.php
+++ View/Pediatrician/pediatrician-viewNotesView.php
@@ -4,7 +4,7 @@ include "../../Assets/Includes/header_pages.php";
 ?>
 <html>
 <head>
-<link rel="stylesheet" href="../../Assets/css/pediatrician-style.css">
+<style><?php include "../../Assets/Css/style-common.css" ?></style>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 </head> 
 <body class="txtcol">
@@ -13,15 +13,19 @@ include "../../Assets/Includes/header_pages.php";
 
 ?>
 
-    <div class="main-dash">
-        <h1> Notes and Records</h1>
-        </div>
-
-    <div class="main-notes">
-    <table class="styled-table">
+<div class="a-container">
+        <div class="a-content">
+            <div class="a-container-n">
+                <h1>View Doctor Notes </h1>
+                <div class="a-container-m">
+                <span class="material-symbols-outlined" style="color: red;">notifications</span>
+                </div></div>
+                
+    <div class="ma-table">
+    <table>
     <thead>
             <tr>
-                <th>Note id</th>
+                <th>Child id</th>
                 <th>Doctor id</th>
                 <th>Mother id</th>
                 <th>Topic</th>
