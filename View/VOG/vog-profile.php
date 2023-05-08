9888df8614049b7fff392f508e093f3175cf74a2
Kivi Amarakoon <89955961+kivi98@users.noreply.github.com>
2023-05-09 00:46:35 +0530

    vog profile updated

--- View/VOG/vog-profile.php
+++ View/VOG/vog-profile.php
@@ -12,6 +12,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style><?php include "../../Assets/css/style-common.css" ?></style>
+    <?php
+        $doc_id = $_SESSION['doc_id'];
+        $doctor_id = '';
+        $doctor_name = '';
+        $doctor_dob = '';
+        $doctor_tele = '';
+        $doctor_email = '';
+        $doctor_workplace = '';
+        $doctor_address = '';
+        $doctor_pic = '';
+        $sql = "SELECT * FROM doctor_details WHERE doc_id = '$doc_id'";
+        $result = mysqli_query($con, $sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+            $doctor_id = $row['doc_id'];
+            $doctor_name = $row['doc_name'];
+            $doctor_dob = $row['doc_dob'];
+            $doctor_tele = $row['doc_tele'];
+            $doctor_email = $row['doc_email'];
+            $doctor_workplace = $row['doc_workplace'];
+            $doctor_address = $row['doc_address'];
+            $doctor_pic = $row['doc_pic'];
+        }
+    ?>
 </head>
 <body>
 <div class="profile-content-mainDiv">
@@ -19,20 +42,8 @@
             <div class="profile-content-innerDiv">
                 <div class="profile-pic">
                     <img src="../../Assets/Images/mother/Profile pic_mom.png" alt="Profile pic_mom">
-                    <div class="profile-child-data">
-                        <hr>
-                        <h3>Children Details</h3>
-                        <div class="child-details-section">
-                            <table>
-                                <tr>
-                                    <td><label for="child-name">Child name</label></td>
-                                    <td><a href="#"><input type="button" value="View" name="view-btn"></a></td>
-                                </tr>
-                            </table>
-                        </div>
-                    </div>
                     <div class="profile-guardian-data">
-                        <h3>Guardian Details</h3>
+                        <h3>Profetional Qualifications</h3>
                         <div class="child-details-section">
                             <table>
                                 <tr>
@@ -49,7 +60,7 @@
                 </div>
                 <div class="profile-details">
                     <div class="profile-name">
-                        <h4><?php  echo $doctor_fname. " " .$doctor_mname. " " .$doctor_lname ;?></h4>
+                        <h4><?php  echo $doctor_name;?></h4>
                     </div>
                     <div class="profile-address">
                         <span class="material-icons">fmd_good</span>
@@ -66,11 +77,7 @@
                             </tr>
                             <tr>
                                 <td><p>Mobile Number</p></td>
-                                <td><label for="mother-mobile"><?php  echo $doctor_mobile?></label></td>
-                            </tr>
-                            <tr>
-                                <td><p>Fixedline Number</p></td>
-                                <td><label for="mother-landline"><?php  echo $doctor_landline?></label></td>
+                                <td><label for="mother-mobile"><?php  echo $doctor_tele?></label></td>
                             </tr>
                             <tr>
                                 <td><p>Date of Birth</p></td>
@@ -80,10 +87,13 @@
                                 <td><p>Email</p></td>
                                 <td><label for="mother-email"><?php  echo $doctor_email?></label></td>
                             </tr>
+                            <tr>
+                                <td><p>Workplace</p></td>
+                                <td><label for="mother-workplace"><?php  echo $doctor_workplace?></label></td>
+                            </tr>
                         </table>
                     </div>
                 </div>
-
             </div>
         </div>
         <div class="profile-buttons">
